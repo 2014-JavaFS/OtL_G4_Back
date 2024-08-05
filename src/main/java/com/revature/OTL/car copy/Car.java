d1d9d46cca025a0e1c2896f9b634ec0eaf67a71e
Tommy Hoang <tommy25hoang@gmail.com>
2024-08-05 17:47:51 -0500

    Cart init

---
+++ src/main/java/com/revature/OTL/car copy/Car.java
@@ -0,0 +1,33 @@
+package com.revature.OTL.car;
+
+import com.revature.OTL.enums.Condition;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String make;
+    private String colour;
+    private String model;
+    private int year;
+    private String trim;
+    private double price;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Condition condition;
+}
+
+
--- src/main/java/com/revature/OTL/user/UserController.java
+++ src/main/java/com/revature/OTL/user/UserController.java
@@ -1,5 +1,7 @@
 package com.revature.OTL.user;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,46 +10,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.OTL.user.impl.UserServiceImpl;
-
 import jakarta.validation.Valid;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-     private final UserService userService;
 
-     @Autowired
-     public UserController(UserService userService) {
-          this.userService = userService;
-     }
+    private final UserService userService;
 
-     /**
-      * g
-      * @param newAppUser
-      * @return ResponseEntity object, response status, and response body
-      */
-     @PostMapping("/register")
-     private ResponseEntity<AppUser> registerUser(@Valid @RequestBody AppUser newAppUser){
-          return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(newAppUser));
-     }
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-     @PostMapping("/login")
-     public ResponseEntity<?> login(@RequestBody Map<String, String> requestBody) {
-          String username = requestBody.get("username");
-          String password = requestBody.get("password");
+    /**
+     * g
+     *
+     * @param newAppUser
+     * @return ResponseEntity object, response status, and response body
+     */
+    @PostMapping("/register")
+    private ResponseEntity<AppUser> registerUser(@Valid @RequestBody AppUser newAppUser) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(newAppUser));
+    }
 
-          boolean isLoggedIn = userService.login(username, password);
-          if (isLoggedIn) {
-               // Successful login, return appropriate response
-               return ResponseEntity.ok("Login successful");
-          } else {
-               // Handle failed login, e.g., return error response
-               return ResponseEntity.status(401).body("Invalid credentials");
-          }
-     }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> requestBody) {
+        String username = requestBody.get("username");
+        String password = requestBody.get("password");
+
+        boolean isLoggedIn = userService.login(username, password);
+        if (isLoggedIn) {
+            // Successful login, return appropriate response
+            return ResponseEntity.ok("Login successful");
+        } else {
+            // Handle failed login, e.g., return error response
+            return ResponseEntity.status(401).body("Invalid credentials");
+        }
+    }
 
 }
-
