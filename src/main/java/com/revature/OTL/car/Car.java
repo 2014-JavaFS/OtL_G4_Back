b5f001201823ca04c4518a5f44bbca340543de11
Tommy Hoang <55764170+Karituo-Luster@users.noreply.github.com>
2024-08-06 10:14:02 -0500

    Update Car.java

--- src/main/java/com/revature/OTL/car/Car.java
+++ src/main/java/com/revature/OTL/car/Car.java
@@ -5,14 +5,7 @@ import java.util.List;
 import com.revature.OTL.cart.Cart;
 import com.revature.OTL.enums.Condition;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
