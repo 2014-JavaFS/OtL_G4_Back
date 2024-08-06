ad13fd04d61ee61f6e72d7439999cc589d63e241
Tommy Hoang <55764170+Karituo-Luster@users.noreply.github.com>
2024-08-06 10:09:52 -0500

    Update AppUser.java

--- src/main/java/com/revature/OTL/user/AppUser.java
+++ src/main/java/com/revature/OTL/user/AppUser.java
@@ -4,14 +4,8 @@ import java.util.List;
 
 import com.revature.OTL.cart.Cart;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
