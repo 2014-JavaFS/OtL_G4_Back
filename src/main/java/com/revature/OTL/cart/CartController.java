5813cdde740a360a5ccd3e1ebafbdc694657a20c
Tommy Hoang <tommy25hoang@gmail.com>
2024-08-06 09:57:18 -0500

    Controller Cleanup

--- src/main/java/com/revature/OTL/cart/CartController.java
+++ src/main/java/com/revature/OTL/cart/CartController.java
@@ -2,6 +2,8 @@ package com.revature.OTL.cart;
 
 import java.util.List;
 
+import com.revature.OTL.cart.dto.CartRequestDto;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/carts")
+@RequestMapping("/carts")
 public class CartController {
 
     private final CartService cartService;
@@ -25,24 +27,24 @@ public class CartController {
     }
 
     @GetMapping
-    public List<Cart> getAllCarts() {
+    public List<CartRequestDto> getAllCarts() {
         return cartService.getAllCarts();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Cart> getCartById(@PathVariable int id) {
+    public ResponseEntity<CartRequestDto> getCartById(@PathVariable int id) {
         return cartService.getCartById(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Cart createCart(@RequestBody Cart cart) {
-        return cartService.createCart(cart);
+    public CartRequestDto createCart(@RequestBody CartRequestDto cartRequestDto) {
+        return cartService.createCart(cartRequestDto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Cart> updateCart(@PathVariable int id, @RequestBody Cart cartDetails) {
+    public ResponseEntity<Cart> updateCart(@PathVariable int id, @RequestBody CartRequestDto cartDetails) {
         return ResponseEntity.ok(cartService.updateCart(id, cartDetails));
     }
 
