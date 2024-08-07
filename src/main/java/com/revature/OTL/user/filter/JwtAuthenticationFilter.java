bc96420cabda0ad58337c996fe2d4ed22e30279d
Tommy Hoang <tommy25hoang@gmail.com>
2024-08-07 13:40:48 -0500

    Second Commit; Servlets Issue

--- src/main/java/com/revature/OTL/user/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/revature/OTL/user/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,42 @@
 package com.revature.OTL.user.filter;
 
-public class JwtAuthenticationFilter {
+import com.revature.OTL.user.impl.UserServiceImpl;
+import com.revature.OTL.user.*;
+import com.revature.OTL.user.config.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserServiceImpl userDetailsService;
+
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            String username = jwtUtil.extractUsername(token);
+
+            if (username != null && jwtUtil.validateToken(token, username)) {
+                AppUser userData = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userData, null, userData.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        chain.doFilter(request, response);
+    }
 }
